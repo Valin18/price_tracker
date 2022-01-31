54b298e60fa344cc40edd825c2709a3c88f815a8
Valin18 <86201485+Valin18@users.noreply.github.com>
2022-01-31 12:45:01 +0300

    Update price_tracker2.py

--- price_tracker2.py
+++ price_tracker2.py
@@ -10,23 +10,26 @@ import sys
 url = 'https://www.hepsiburada.com/xiaomi-redmi-note-10s-128-gb-6-gb-ram-xiaomi-turkiye-garantili-p-HBCV00000FO61M'
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 OPR/82.0.4227.50'}
 
-
-
+page = requests.get(url,headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+span = soup.find(id='offering-price')
+content = span.attrs.get('content')
+price = float(content)
 
 # Gmail email sunucusuna bağlanıyoruz
 def send_mail(title):
     try:
-        mail = smtplib.SMTP("smtp.gmail.com",587)
+        mail = smtplib.SMTP("smtp.gmail.com", 587)
         mail.ehlo()
         mail.starttls()
-        mail.login('yigitefeps4@gmail.com', 'Efe19033')
+        mail.login('pricetracking11@gmail.com', 'jwdlsvhkncwinqpq')
 
         mesaj = MIMEMultipart()
-        mesaj["From"] = "yigitefeps4@gmail.com"          # Gönderen
+        mesaj["From"] = "pricetracking11@gmail.com"          # Gönderen
         mesaj['To'] = "gamerefe7788@gmail.com"           # Gönderilen
         mesaj["Subject"] = "Note 10 S Fiyat Güncellemesi"    # Konusu
 
-        body = "Note 10 S Fiyatı 4000 Altına Düştü ! \n\nŞu Linkten Ulaşabilirsiniz => " + url
+        body = 'Note 10 S Güncel Fiyatı '+ content +'\n\n Şu Linkten Ulaşabilirsiniz => ' + url
 
 
 
